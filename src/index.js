c73a533c6f2523c036c6fa71a8af228bacdc8a21
Zoshua Colah <zcolah@D-173-250-173-137.dhcp4.washington.edu>
2016-12-03 16:11:01 -0800

    Sign In and Sign Up work perfectly ... we need to now figure out Routing

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router';
-import App from './App';
+import App, {NewsFeed, Stats} from './App';
+import Join from './join'
+import Login from './login'
 import firebase from 'firebase';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDADRuBNTKL9k68ezcRssLOsSrWY8maeqE",
-  authDomain: "info-343a-final.firebaseapp.com",
-  databaseURL: "https://info-343a-final.firebaseio.com",
-  storageBucket: "info-343a-final.appspot.com",
-  messagingSenderId: "327840590763"
+  apiKey: "AIzaSyAfwrtHv5IjhHux7o19pk5C9LR5gIqaKhs",
+  authDomain: "reactnews-51fe8.firebaseapp.com",
+  databaseURL: "https://reactnews-51fe8.firebaseio.com",
+  storageBucket: "reactnews-51fe8.appspot.com",
+  messagingSenderId: "620524973308"
 };
+
 firebase.initializeApp(config);
 
 ReactDOM.render(
-   <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      {<IndexRoute component={App} />/*
-      <Route path="channels" component={MainPage} />
-      <Route path="channel/:name" component={MessageList} />
-      <Route path="login" component={SignInForm} />
-      <Route path="join" component={SignUpForm} />*/}
+  <Router history={hashHistory}>
+    <Route path="/" component={App} >
+      <Route path="join" component={Join} />
+      <Route path="login" component={Login} />
+      {/* <IndexRoute component={NewsFeed} />
+      <Route path="newsfeed" component={NewsFeed} />
+      <Route path="stats" component={Stats}*/}
     </Route>
-  </Router>,
-  document.getElementById('root')
+  </Router>,  document.getElementById('root')
 );
+
+firebase.auth();
---
+++ src/login.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import noUserPic from './img/no-user-pic.png';
+
+class SignInForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'email': undefined,
+      'password': undefined,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    var field = event.target.name;
+    var value = event.target.value;
+    var changes = {}; //object to hold changes
+    changes[field] = value;
+    this.setState(changes); //update state
+  }
+  signIn(event) {
+    event.preventDefault(); //don't submit
+    this.props.signInCallback(this.state.email, this.state.password);
+  }
+  validate(value, validations) {
+    var errors = { isValid: true, style: '' };
+    if (value !== undefined) { //check validations
+      if (validations.required && value === '') {
+        errors.required = true;
+        errors.isValid = false;
+      }
+      if (validations.minLength && value.length < validations.minLength) {
+        errors.minLength = validations.minLength;
+        errors.isValid = false;
+      }
+      if (validations.email) {
+        var valid = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)
+        if (!valid) {
+          errors.email = true;
+          errors.isValid = false;
+        }
+      }
+    }
+    if (!errors.isValid) {
+      errors.style = 'has-error';
+    }
+    else {
+      errors.isValid = false;
+    }
+    return errors; //return data object
+  }
+  render() {
+    var emailErrors = this.validate(this.state.email, { required: true, email: true });
+    var passwordErrors = this.validate(this.state.password, { required: true, minLength: 6 });
+    var handleErrors = this.validate(this.state.handle, { required: true, minLength: 3 });
+    var signInEnabled = (emailErrors.isValid && passwordErrors.isValid);
+    return (
+      <form role="form" className="sign-up-form">
+        <ValidatedInput field="email" type="email" label="Email" changeCallback={this.handleChange} errors={emailErrors} />
+        <ValidatedInput field="password" type="password" label="Password" changeCallback={this.handleChange} errors={passwordErrors} />
+        <div className="form-group sign-up-buttons">
+          <button className="btn btn-primary" disabled={!signInEnabled} onClick={(e) => this.signIn(e)}>Sign-in</button>
+        </div>
+      </form>
+    );
+  }
+}
+SignInForm.propTypes = {
+  signInCallback: React.PropTypes.func.isRequired
+};
+
+class ValidatedInput extends React.Component {
+  render() {
+    return (
+      <div className={"form-group " + this.props.errors.style}>
+        <label htmlFor={this.props.field} className="control-label">{this.props.label}</label>
+        <input id={this.props.field} type={this.props.type} name={this.props.field} className="form-control" onChange={this.props.changeCallback} />
+        <ValidationErrors errors={this.props.errors} />
+      </div>
+    );
+  }
+}
+
+class ValidationErrors extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.errors.required &&
+          <p className="help-block">Required!</p>
+        }
+        {this.props.errors.email &&
+          <p className="help-block">Not an email address!</p>
+        }
+        {this.props.errors.minLength &&
+          <p className="help-block">Must be at least {this.props.errors.minLength}characters.</p>
+        }
+      </div>
+    );
+  }
+}
+
+class SignInApp extends React.Component {
+  signIn(email, password) {
+    window.alert("Signing in:", email);
+  }
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Sign In!</h1>
+        </header>
+        <SignInForm signInCallback={this.signIn} />
+      </div>
+    );
+  }
+}
+
+export default SignInForm;
