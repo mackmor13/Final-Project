ac54a1a349e5cada7d0b8e5b6b6e6b1a4b822098
mackmor13 <mackaay@uw.edu>
2016-12-14 21:16:15 -0800

    updated package.json

--- src/stats.js
+++ src/stats.js
@@ -1,20 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @mackmor13
- Watch 2
-  Star 0
- Fork 0 mackmor13/finalProject
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
-Branch: resubmission Find file Copy pathfinalProject/src/stats.js
-15f89f9  13 minutes ago
-@jtpraino jtpraino removed firebase work from render in stats.js
-2 contributors @jtpraino @mackmor13
-RawBlameHistory     
-61 lines (54 sloc)  2.04 KB
 import React from 'react';
 import './style.css';
 import firebase from 'firebase';
@@ -76,5 +59,3 @@ class Stats extends React.Component {
 }
 
 export default Stats;
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
